acde4e3ef5ec1ea2a9a7e6f1efe705c54f2b29af
Ruben Sardon <ec4who@hotmail.com>
2017-09-23 15:33:02 +0200

    Added ModuleWindowsFS (PhysFS wasn't working)
    ModuleWindowsFS uses windows' fstream and sstream to read from
    Configuration.json

--- ModuleFS.cpp
+++ ModuleFS.cpp
@@ -7,14 +7,69 @@
 
 #pragma comment( lib, "PhysFS/libx86/physfs.lib" )
 
-ModuleFS::ModuleFS(bool start_enabled) : Module(start_enabled)
+#include <fstream>
+#include <sstream>
+
+ModuleWindowsFS::ModuleWindowsFS(const char* name, bool start_enabled) : Module(name, start_enabled)
+{}
+
+ModuleWindowsFS::~ModuleWindowsFS()
+{}
+
+bool ModuleWindowsFS::LoadFileToBuffer(char** buffer, const char* file) const
+{
+	bool ret = false;
+
+	std::ifstream ifs;
+	ifs.open(file, std::ifstream::in);
+
+	if (!ifs.is_open())
+	{
+		LOG("WindowsFS Error - could't OPEN %s\n", file);
+	}
+	else
+	{
+		std::stringstream iss;
+		iss << ifs.rdbuf();
+
+		iss.seekg(0, std::ios::end); // set stringstream position to file end
+		uint size = iss.tellg(); // get size by getting offset position
+
+		if (size == 0)
+		{
+			LOG("WindowsFS Error - could't DETERMINE SIZE for %s\n", file);
+		}
+		else
+		{
+			*buffer = new char[size];
+			iss.str()._Copy_s(*buffer, size, size);
+			ret = true;
+		}
+
+		ifs.close();
+	}
+
+	return ret;
+}
+
+void ModuleWindowsFS::Save(const char* file, const void* buffer, unsigned int size) const
+{
+
+}
+
+
+
+
+
+ModuleFS::ModuleFS(const char* name, bool start_enabled) : Module(name, start_enabled)
 {
 	char* path = SDL_GetBasePath();
 	LOG(path);
-	SDL_assert_paranoid(PHYSFS_init(path) != 0);
+	SDL_assert(PHYSFS_init(path) != 0);
 	SDL_free(path);
 
-	//AddPath(".");
+	// Add own directory to find Configuration.json later
+	SDL_assert(AddPath("/"));
 }
 
 ModuleFS::~ModuleFS()
@@ -39,8 +94,8 @@ PHYSFS_sint64 ModuleFS::LoadFileToBuffer(char** buffer, const char* file) const
 {
 	PHYSFS_sint64 ret = -1;
 
-	if (file == nullptr || PHYSFS_exists(file) == NULL)
-		return ret;
+	/*if (file == nullptr || PHYSFS_exists(file) == NULL)
+		return ret;*/
 
 	PHYSFS_file* fs_file = PHYSFS_openRead(file);
 
@@ -75,6 +130,38 @@ PHYSFS_sint64 ModuleFS::LoadFileToBuffer(char** buffer, const char* file) const
 		}
 
 		if (PHYSFS_close(fs_file) <= 0)
+		{
+			LOG("FS Error - couldn't CLOSE %s: %s\n", file, PHYSFS_getLastError());
+		}
+	}
+
+	return ret;
+}
+
+PHYSFS_sint64 ModuleFS::Save(const char* file, const void* buffer, unsigned int size) const
+{
+	PHYSFS_sint64 ret = -1;
+
+	PHYSFS_file* fs_file = PHYSFS_openWrite(file);
+
+	if (fs_file == NULL)
+	{
+		LOG("FS Error - could't OPEN for saving %s: %s\n", file, PHYSFS_getLastError());
+	}
+	else
+	{
+		PHYSFS_sint64 written = PHYSFS_write(fs_file, (const void*)buffer, 1, size);
+
+		if (written != size)
+		{
+			LOG("File System error while writing to file %s: %s\n", file, PHYSFS_getLastError());
+		}
+		else
+		{
+			ret = written;
+		}
+
+		if (PHYSFS_close(fs_file) == 0)
 		{
 			LOG("FS Error - couldn't CLOSE %s: %s\n", file, PHYSFS_getLastError());
 		}
--- ModuleFS.h
+++ ModuleFS.h
@@ -4,15 +4,26 @@
 #include "Module.h"
 #include "PhysFS/include/physfs.h"
 
+class ModuleWindowsFS : public Module
+{
+public:
+	ModuleWindowsFS(const char* name, bool start_enabled = true);
+	~ModuleWindowsFS();
+
+	bool LoadFileToBuffer(char** buffer, const char* file = nullptr) const;
+	void Save(const char* file, const void* buffer, unsigned int size) const;
+};
+
+
 class ModuleFS : public Module
 {
 public:
-	ModuleFS(bool start_enabled = true);
+	ModuleFS(const char* name, bool start_enabled = true);
 	~ModuleFS();
 
 	bool AddPath(const char* path, const char* mount_point = nullptr);
 	PHYSFS_sint64 LoadFileToBuffer(char** buffer, const char* file = nullptr) const;
+	PHYSFS_sint64 Save(const char* file, const void* buffer, unsigned int size) const;
 };
-
 
 #endif
