a740c48467b10ae2f2380495c412b8034a38f856
Ruben Sardon <ec4who@hotmail.com>
2017-09-27 08:48:40 +0200

    Added Restart Main handling
    Crashes completly and freezes VS

--- Main.cpp
+++ Main.cpp
@@ -18,6 +18,7 @@ enum main_states
 	MAIN_CREATION,
 	MAIN_START,
 	MAIN_UPDATE,
+	MAIN_RESTART,
 	MAIN_FINISH,
 	MAIN_EXIT
 };
@@ -27,24 +28,18 @@ Application* App = nullptr;
 
 int main(int argc, char ** argv)
 {
-	LOG("Starting game '%s'...", TITLE);
-
-	
-
 	int main_return = EXIT_FAILURE;
 	main_states state = MAIN_CREATION;
+	int update_return = UPDATE_CONTINUE;
 
 	while (state != MAIN_EXIT)
 	{
-		
-
 		switch (state)
 		{
 		case MAIN_CREATION:
 
 			LOG("-------------- Application Creation --------------");
 			App = new Application();
-			App->time->SetMaxFPS(60);
 			state = MAIN_START;
 			break;
 
@@ -65,18 +60,43 @@ int main(int argc, char ** argv)
 			break;
 
 		case MAIN_UPDATE:
-		{
-			int update_return = App->Update();
+
+			update_return = App->Update();
 
 			if (update_return == UPDATE_ERROR)
 			{
 				LOG("Application Update exits with ERROR");
 				state = MAIN_EXIT;
 			}
-
-			if (update_return == UPDATE_STOP)
+			else if (update_return == UPDATE_STOP)
+			{
 				state = MAIN_FINISH;
-		}
+			}
+			else if (update_return == UPDATE_RESTART)
+			{
+				state = MAIN_RESTART;
+			}
+
+			break;
+
+		case MAIN_RESTART:
+
+			update_return = App->Restart();
+
+			if (update_return == UPDATE_ERROR)
+			{
+				LOG("Application Restart exits with ERROR");
+				state = MAIN_EXIT;
+			}
+			else if (update_return == UPDATE_STOP)
+			{
+				state = MAIN_FINISH;
+			}
+			else if (update_return == UPDATE_CONTINUE)
+			{
+				state = MAIN_UPDATE;
+			}
+
 			break;
 
 		case MAIN_FINISH:
@@ -90,14 +110,12 @@ int main(int argc, char ** argv)
 				main_return = EXIT_SUCCESS;
 
 			state = MAIN_EXIT;
-
 			break;
 
 		}
 	}
 
 	delete App;
-	LOG("Exiting game '%s'...\n", TITLE);
 
 #ifdef _DEBUG
 	int leaks = MAX(0, m_getMemoryStatistics().totalAllocUnitCount - 23);
